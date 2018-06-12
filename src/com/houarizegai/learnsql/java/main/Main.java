eeb99a848672c4441f57009627e4e7b607acfe73
Houari Zegai <houarizegai14@gmail.com>
2018-06-12 16:31:24 +0100

    Uploading database

--- src/com/houarizegai/learnsql/java/main/Main.java
+++ src/com/houarizegai/learnsql/java/main/Main.java
@@ -13,7 +13,7 @@ public class Main extends Application {
     
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("/com/houarizegai/learnsql/resources/views/Login.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("/com/houarizegai/learnsql/resources/views/System.fxml"));
         Scene scene = new Scene(root);
         stage.setScene(scene);
         stage.setTitle("LSQL Application");
