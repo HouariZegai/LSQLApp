1015f22d1f359fe60ffc77df4f3f6aace0d4b0e0
HouariZEGAI <houarizegai14@gmail.com>
2019-03-11 15:14:07 +0100

    Adding grades front & back (coming soon Insh'Allah)

---
+++ src/com/houarizegai/learnsql/java/controllers/GradesController.java
@@ -0,0 +1,13 @@
+package com.houarizegai.learnsql.java.controllers;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+public class GradesController implements Initializable {
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+
+    }
+}
